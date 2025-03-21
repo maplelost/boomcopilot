11b2dbdc32fd49d55a82f2aed482d047659ec2d4
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-03-21 15:22:30 +0800

    背景色

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
 import Popup from './views/Popup.vue'
-
 </script>
 
 <template>
-  <div class="main-window-container">
+  <div class="app-container">
     <Popup />
   </div>
 </template>
-
-<style scoped lang="scss">
-.main-window-container {
-  width: 100%;
-  height: 100%;
-}
-</style>
