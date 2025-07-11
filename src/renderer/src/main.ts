0a6a352a5d08b7ea89237b0b8766803e6f73ca1f
maplelost <62323046+maplelost@users.noreply.github.com>
2025-07-11 16:59:15 +0800

    naive-ui

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,9 +2,9 @@ import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import naive from 'naive-ui'
 import App from './App.vue'
-import router from './router'
+import router from '@/router'
+import naive from '@/plugins/naive-ui'
 
 const app = createApp(App)
 
